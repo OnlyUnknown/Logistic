fc04055bbaf221af881ec612150dd3dfd01bafb4
OnlyUnknown <abdullahkhrief@gmail.com>
2024-02-04 16:51:58 +0300

    have rixed linters'

--- app/controllers/api/v1/supervisors_controller.rb
+++ app/controllers/api/v1/supervisors_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::SupervisorsController < ApplicationController
-    def index
-        @supervisor = Supervisor.find(params[:id])
-        if @supervisor
-          render json: @supervisor
-        else
-          render json: @supervisor.errors.full_messages
-        end
-      end
+  def index
+    @supervisor = Supervisor.find(params[:id])
+    if @supervisor
+      render json: @supervisor
+    else
+      render json: @supervisor.errors.full_messages
+    end
+  end
 end
