538f59ea2a78b966e6d2211c0f4e432547af2b39
OnlyUnknown <abdullahkhrief@gmail.com>
2024-02-06 13:32:52 +0300

    have fixed linters

--- app/controllers/api/v1/supervisions_controller.rb
+++ app/controllers/api/v1/supervisions_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::SupervisionsController < ApplicationController
+  def index
+    @supervision = Supervision.all
+    if @supervision
+      render json: @supervision
+    else
+      render json: @supervision.errors.full_messages
+    end
+  end
 end
