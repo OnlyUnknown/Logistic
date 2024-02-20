9a40e71fb53ddaee0426ae6b44aa2c60b21a5da0
OnlyUnknown <abdullahkhrief@gmail.com>
2024-02-20 14:55:49 +0300

    have made the id to be genrated and being unique

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -10,6 +10,13 @@ class Api::V1::TasksController < ApplicationController
       end
     
       private
+
+      def generate_task_id
+        loop do
+          random_id = SecureRandom.alphanumeric(12).upcase
+          break self.id = random_id unless Task.exists?(id: random_id)
+        end
+      end
     
       def task_params
         params.require(:task).permit(:supervisor_id, :product, :sender, :quantity, :receiver, :status, :price, :current_location, :total,)
--- db/migrate/20240214131115_create_tasks.rb
+++ db/migrate/20240214131115_create_tasks.rb
@@ -1,6 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
+      t.index :id, unique: true
       t.references :supervisor, null: false, foreign_key: true
       t.string :product
       t.string :sender
