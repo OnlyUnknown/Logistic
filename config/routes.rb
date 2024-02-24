b070a99f1cd8fee4f90970ecfdad1cedab99938e
OnlyUnknown <abdullahkhrief@gmail.com>
2024-02-24 18:57:39 +0300

    have initialized the devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
+      root 'agents#index'
       get 'agent/:id', to: 'agents#index'
       get 'supervisor/:id', to: 'supervisors#index'
       get 'customer/:id', to: 'customers#index'
