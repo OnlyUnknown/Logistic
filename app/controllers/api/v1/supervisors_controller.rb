63943855865f5546be32428668d7656466d74109
OnlyUnknown <abdullahkhrief@gmail.com>
2024-03-17 00:38:42 +0300

    have created the create task funciton

--- app/controllers/api/v1/supervisors_controller.rb
+++ app/controllers/api/v1/supervisors_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::SupervisorsController < ApplicationController
+  before_action :authenticate_supervisor!, only: [:create_task]
   def index
     @supervisor = Supervisor.find(params[:id])
     if @supervisor
@@ -6,5 +7,8 @@ class Api::V1::SupervisorsController < ApplicationController
     else
       render json: @supervisor.errors.full_messages
     end
+  end
+  def create_task
+    @new_task = Task.create()
   end
 end
