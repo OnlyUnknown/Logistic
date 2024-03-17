5be977439aadc39f635f757cb27e76ef677db73f
OnlyUnknown <abdullahkhrief@gmail.com>
2024-03-17 05:50:30 +0300

    have created a function to create a random number for the taks id

--- app/controllers/api/v1/supervisors_controller.rb
+++ app/controllers/api/v1/supervisors_controller.rb
@@ -1,26 +1,38 @@
 class Api::V1::SupervisorsController < ApplicationController
   before_action :authenticate_supervisor!, only: [:create_task]
+
   def index
     @supervisor = current_supervisor
     if @supervisor
       render json: @supervisor
     else
-      render json: @supervisor.errors.full_messages
+      render json: { error: "Supervisor not found" }, status: :not_found
     end
   end
+
   def create_task
     @task = Task.new(task_params)
 
     if @task.save
       render json: @task
     else
-      render 'new'
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
     end
   end
+
   private
   
   def task_params
     supervisor = current_supervisor
-    params.require(:task).permit(:product, :quantity, :price, :total).merge(status:"pending",supervisor: supervisor)
+    random_id = generate_task_id
+    params.require(:task).permit(:product, :quantity, :price, :total).merge(id: random_id, status: "pending", supervisor: supervisor)
+  end
+
+  def generate_task_id
+    loop do
+      timestamp = rand 100000000000...999999999999
+    random_id = "a#{timestamp}"
+    return random_id unless Task.exists?(id: random_id)
+    end
   end
 end
