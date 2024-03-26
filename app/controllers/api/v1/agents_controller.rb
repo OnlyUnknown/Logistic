6be6683034cecb3c2ecfa1524f917d3de1274dd2
OnlyUnknown <abdullahkhrief@gmail.com>
2024-03-26 18:53:30 +0300

    have added the functionality to remove the task from the agent itself

--- app/controllers/api/v1/agents_controller.rb
+++ app/controllers/api/v1/agents_controller.rb
@@ -26,15 +26,17 @@ class Api::V1::AgentsController < ApplicationController
   def remove_task
     @task = Task.find(params[:id])
     check_supervision(@task.supervisor_id)
-    if @task.agent_id != nil
-       @task.update(agent_id: nil, status: "pending") unless @task.agent_id == current_agent.id
-       render json: { message: 'Task removed successfully' }
-     else
-       render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
-
-   end
-    
-    
+  
+    if @task.agent_id.nil?
+      render json: { errors: 'Agent ID is already nil' }, status: :unprocessable_entity
+    else
+      if current_agent.id == @task.agent_id
+        @task.update(agent_id: nil, status: "pending") 
+        render json: { message: 'Task removed successfully' }
+      else
+        render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
   end
   private
 
