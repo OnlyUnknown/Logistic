3633556f94be8e3778122221c8e984ae4b0c147a
OnlyUnknown <abdullahkhrief@gmail.com>
2024-03-30 18:45:38 +0300

    have fixed linters

--- app/controllers/api/v1/agents_controller.rb
+++ app/controllers/api/v1/agents_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::AgentsController < ApplicationController
-  before_action :authenticate_agent!, only: [:accept_task, :remove_task, :mytasks_list, :my_supervisors, :my_supervisors_tasks]
-  
+  before_action :authenticate_agent!,
+                only: %i[accept_task remove_task mytasks_list my_supervisors my_supervisors_tasks]
+
   def index
     @agent = current_agent
     if @agent
@@ -14,11 +15,11 @@ class Api::V1::AgentsController < ApplicationController
     @list = Task.where(agent_id: current_agent.id)
     render json: @list
   end
-  
+
   def my_supervisors
     @supervisors = Supervision.where(agent_id: current_agent.id).pluck(:supervisor_id)
-  @s = Supervisor.where(id: @supervisors).select(:id, :name, :phone_number)
-  render json: @s
+    @s = Supervisor.where(id: @supervisors).select(:id, :name, :phone_number)
+    render json: @s
   end
 
   def my_supervisors_tasks
@@ -26,17 +27,16 @@ class Api::V1::AgentsController < ApplicationController
     @s = Supervisor.includes(:tasks).where(id: @supervisors).select(:id, :name, :phone_number)
     render json: @s, include: :tasks
   end
-  
 
   def accept_task
     @task = Task.find(params[:id])
     check_supervision(@task.supervisor_id)
-    
+
     if @task.agent_id.nil?
-       @task.update(agent_id: current_agent.id, status: "on delivery")
-        render json: { message: 'Task accepted successfully' }
-      else
-        render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+      @task.update(agent_id: current_agent.id, status: 'on delivery')
+      render json: { message: 'Task accepted successfully' }
+    else
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
 
     end
   end
@@ -44,24 +44,22 @@ class Api::V1::AgentsController < ApplicationController
   def remove_task
     @task = Task.find(params[:id])
     check_supervision(@task.supervisor_id)
-  
+
     if @task.agent_id.nil?
       render json: { errors: 'Agent ID is already nil' }, status: :unprocessable_entity
+    elsif current_agent.id == @task.agent_id
+      @task.update(agent_id: nil, status: 'pending')
+      render json: { message: 'Task removed successfully' }
     else
-      if current_agent.id == @task.agent_id
-        @task.update(agent_id: nil, status: "pending") 
-        render json: { message: 'Task removed successfully' }
-      else
-        render json: { errors: "Something went wrong" }, status: :unprocessable_entity
-      end
+      render json: { errors: 'Something went wrong' }, status: :unprocessable_entity
     end
   end
+
   private
 
   def check_supervision(supervisor_id)
-    return if Supervision.exists?(supervisor_id:supervisor_id, agent_id:current_agent.id)
+    return if Supervision.exists?(supervisor_id:, agent_id: current_agent.id)
 
     raise ActiveRecord::RecordNotDestroyed, "The task isn't under you supervisors"
   end
-
 end
