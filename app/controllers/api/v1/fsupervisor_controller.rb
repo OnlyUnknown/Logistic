13dba6e00c08fc66a4280586b86e1d540139737b
OnlyUnknown <abdullahkhrief@gmail.com>
2024-05-10 13:08:30 +0300

    have migrated the schema and have created the controller for supervisor follow function

---
+++ app/controllers/api/v1/fsupervisor_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::FsupervisorController < ApplicationController
+    before_action :authenticate_supervisor!
+    
+    def add(@user)
+    end
+
+    def is_added(@user)
+    end
+
+    def remove
+    end
+
+    def add_counts
+    end
+
+
+
+end
